e2491fedcffa5b29657cc4783f4b8adc095c571a
Anton Timchenko <antontimchenko1999@gmail.com>
2022-07-22 02:15:30 +0300

    getting started

---
+++ src/pages/index.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+type TechnologyCardProps = {
+  name: string;
+  description: string;
+  documentation: string;
+};
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <Head>
+        <title>Kochalka</title>
+        <meta name="description" content="Exercise platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className="flex text-slate-400 h-[72px] min-h-[72px]">
+        <img src="#" alt="logo" />
+        <nav>
+          <ul className="flex gap-4">
+            <li><a href="#about" className="hover:text-white">About</a></li>
+            <li><a href="#Services" className="hover:text-white">Services</a></li>
+            <li><a href="#Contact Us" className="hover:text-white">Contact Us</a></li>
+            <li><a href="/signin" className="hover:text-white">Sign In</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 selection:bg-lime-300 selection:text-lime-900">
+        <section className="flex lg:grid lg:grid-cols-2  lg:gap-20 lg:items-center">
+          <div>
+          <h1 className="text-6xl md:text-[5rem] font-extrabold text-lime-100 mb-5 leading-[77px]">
+            Exercise as you wish.
+          </h1>
+          <p className="text-2xl text-slate-400 mb-16">Get access to our limitless knowledge base with over 150+ thousand workout programs created by professionals today!</p>
+          <button className="px-8 py-3 text-black font-bold uppercase bg-gradient-to-tr from-lime-500 to-lime-400 hover:bg-lime-800 rounded-md">get started</button>
+          </div>
+          <div>
+          <img src="./lightning.svg" alt="green lightning icon" className="hidden lg:block absolute z-10"/>
+          <img src="./lightning-bg.svg" alt="" className="hidden lg:block"/>
+          </div>
+        </section>
+        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
