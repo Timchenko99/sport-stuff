a17659a1cdaf5b9aef2fd05fec6ac57924e45308
Anton Timchenko <antontimchenko1999@gmail.com>
2022-07-23 00:31:12 +0300

    refactor(home): finish home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
@@ -19,10 +14,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="flex text-slate-400 h-[72px] min-h-[72px]">
-        <img src="#" alt="logo" />
+      <header className="flex items-center justify-center text-gray-400 h-[72px] min-h-[72px]">
+        {/* <img src="#" alt="logo" /> */}
         <nav>
-          <ul className="flex gap-4">
+          <ul className="flex gap-5">
             <li><a href="#about" className="hover:text-white">About</a></li>
             <li><a href="#Services" className="hover:text-white">Services</a></li>
             <li><a href="#Contact Us" className="hover:text-white">Contact Us</a></li>
@@ -31,23 +26,21 @@ const Home: NextPage = () => {
         </nav>
       </header>
 
-      <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4 selection:bg-lime-300 selection:text-lime-900">
-        <section className="flex lg:grid lg:grid-cols-2  lg:gap-20 lg:items-center">
+      <main className="container mx-auto flex flex-col items-center justify-center h-[calc(100vh-72px)] p-4 selection:bg-lime-300 selection:text-lime-900">
+        <section className="flex lg:grid lg:grid-cols-2 lg:gap-20 lg:place-items-center">
           <div>
           <h1 className="text-6xl md:text-[5rem] font-extrabold text-lime-100 mb-5 leading-[77px]">
             Exercise as you wish.
           </h1>
-          <p className="text-2xl text-slate-400 mb-16">Get access to our limitless knowledge base with over 150+ thousand workout programs created by professionals today!</p>
-          <button className="px-8 py-3 text-black font-bold uppercase bg-gradient-to-tr from-lime-500 to-lime-400 hover:bg-lime-800 rounded-md">get started</button>
+          <p className="text-2xl text-gray-400 mb-16">Get access to our limitless knowledge base with over 150+ thousand workout programs created by professionals today!</p>
+          <Link href='/signup'><button className="shadow-md shadow-lime-400/50 hover:shadow-lg hover:shadow-lime-500/40 px-8 py-3 text-neutral-900 font-bold uppercase bg-gradient-to-tr from-lime-500 to-lime-400 hover:from-lime-600 hover:to-lime-700 rounded-md">get started</button></Link>
           </div>
-          <div>
-          <img src="./lightning.svg" alt="green lightning icon" className="hidden lg:block absolute z-10"/>
-          <img src="./lightning-bg.svg" alt="" className="hidden lg:block"/>
+          <div className="relative">
+
+            <img src="./lightning.svg" alt="green lightning icon" className="hidden absolute top-0 left-[3.25rem] lg:block z-10"/>
+            <img src="./lightning-bg.svg" alt="" className=" hidden lg:block"/>
           </div>
         </section>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
       </main>
     </>
   );
